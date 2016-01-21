e13396c785b6a86ddf1b6bceba726ddf162cf2a4
Cruppelt <chris.ruppelt1@gmail.com>
2016-01-20 22:32:14 -0500

    Test 2

---
+++ BAC_App/DataModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BAC_App
+{
+	public class DataModel
+	{
+		public DataModel ()
+		{
+		}
+
+	}
+
+	//Object for Drinks
+	public class Drink
+	{
+		public string name { get; set; }
+		public int alcocholContent { get; set; } //multiply by .01 to get percent?
+		public string type { get; set; } //type of drink Ex. beer or hard liquor
+		public DateTime dateCreated { get; set; }
+	}
+}
+
