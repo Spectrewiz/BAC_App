bbafa618b719da3ac37ba95311643efde3372e8f
Cruppelt <chris.ruppelt1@gmail.com>
2016-01-20 22:30:50 -0500

    Testing changes

--- BAC_App/Database.cs
+++ BAC_App/Database.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using SQLite;
+﻿using System.Collections.Generic;
 
 #if __IOS__
 using Foundation;
@@ -12,19 +10,50 @@ namespace BAC_App
 	{
 		public Database ()
 		{
+			CreateDatabase ();
 		}
 
+		//Creates the database in the given path depending on OS.
 		public static string documentsFolder()
 		{
 			string path;
 
 			#if __ANDROID__
-			path = Android.App.Application.Context.GetExternalFilesDir(null).AbsoluteFile;
+			path = Android.App.Application.Context.GetExternalFilesDir(null).AbsolutePath;
 			#endif
 
 			Directory.CreateDirectory (path);
+			return path;
 		}
 
+		//Creates the database with a table of Drinks
+		public static void CreateDatabase()
+		{
+			var conn = new SQLiteConnection (System.IO.Path.Combine (documentsFolder(), "database.db"));
+			conn.CreateTable <Drink> ();
+			conn.Close ();
+		}
+
+		public static void insertDrink(Drink drink)
+		{
+			var conn = new SQLiteConnection (System.IO.Path.Combine (documentsFolder(), "database.db"));
+			conn.Insert (drink);
+			conn.Close ();
+		}
+
+		public static void deleteDrink(Drink drink)
+		{
+			var conn = new SQLiteConnection (System.IO.Path.Combine (documentsFolder(), "database.db"));
+			conn.Query<Drink> ("DELETE * FROM Drink WHERE_dateCreated=?", drink.dateCreated);
+			conn.Close ();
+		}
+
+		public static List<Drink> getDrinks()
+		{
+			var conn = new SQLiteConnection (System.IO.Path.Combine (documentsFolder(), "database.db"));
+			var results = conn.Query<Drink> ("SELECT * FROM Drink");
+			conn.Close ();
+			return results;
+		}
 	}
 }
-
