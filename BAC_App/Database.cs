0a7d908c2c2d2898268300953ae0e8bc29b3ec6a
Cruppelt <chris.ruppelt1@gmail.com>
2016-01-20 19:51:18 -0500

    Database.cs changes

---
+++ BAC_App/Database.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using SQLite;
+
+#if __IOS__
+using Foundation;
+#endif
+
+namespace BAC_App
+{
+	public class Database
+	{
+		public Database ()
+		{
+		}
+
+		public static string documentsFolder()
+		{
+			string path;
+
+			#if __ANDROID__
+			path = Android.App.Application.Context.GetExternalFilesDir(null).AbsoluteFile;
+			#endif
+
+			Directory.CreateDirectory (path);
+		}
+
+	}
+}
+
